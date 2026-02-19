706a57c8e3ce2b507e3d0dd4b513e3d3c7ac7a04
Владимир Тимофеев <mr.vufik@gmail.com>
2026-02-20 00:58:28 +0300

    vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,15 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      // Все запросы, начинающиеся с /api, Vite перенаправит на Go
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
 })
